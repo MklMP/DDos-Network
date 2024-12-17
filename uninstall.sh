65c3dbb4469c444c8977c1ed4021f96e7f2e8adc
MklMP <maykelmillan96@gmail.com>
2024-12-16 21:28:38 -0500

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+
+echo -e "\n Desintalando DDos_Network..."
+
+echo -e "Borrando rockyou.txt"
+rm /usr/local/bin/rockyou.txt 
+echo -e "Borrando DDos_Bluetooth.sh"
+rm /usr/local/bin/DDos_Bluetooth.sh 
+echo -e "Borrando GeneratorPass.sh"
+rm /usr/local/bin/GeneratorPass.sh
+echo -e "Borrando DDos_Wifi.sh"
+rm /usr/local/bin/DDos_Wifi.sh
+echo -e "Main.sh"
+rm /usr/local/bin/DDosNetwork
+echo -e "SelectionPass.sh"
+rm /usr/local/bin/SelectionPass.sh
+echo -e "DefaultPass.sh"
+rm /usr/local/bin/DefaultPass.sh
+echo -e "FAP.sh" 
+rm /usr/local/bin/FAP.sh 
+echo -e "NMCLI.txt"
+rm /usr/local/bin/NMCLI.txt
+echo -e "DDos_Network.tar.gz"
+rm /usr/local/bin/DDos_Network.tar.gz
+echo -e "DDos_Network"
+rm -r /usr/local/bin/DDos_Network
+echo -e "MACBLUETOOTH.txt"
+rm /usr/local/bin/MACBLUETOOTH.txt
+echo -e "PasswordsFound.txt"
+rm /usr/local/bin/PasswordsFound.txt
+echo -e "rockyou.txt"
+rm /usr/local/bin/rockyou.txt
+echo -e "README.md"
+rm /usr/local/bin/README.md
